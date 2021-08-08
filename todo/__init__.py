da8d2ed9d9ce5ea71192b409f412fb42a86d55be
Hec98 <hector982015@gmail.com>
2021-08-07 23:45:40 -0500

    Add todo/__init__.py and definition of environment variables

---
+++ todo/__init__.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY = 'myKey',
+        DATABASE_HOST = os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_PASSWORD = os.environ.get('FLASK_DATABASE_PASSWORD'),
+        DATABASE_USER = os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE = os.environ.get('FLASK_DATABASE')
+    )
+
+    @app.route('/hello')
+    def hello(): return 'Hello World'
+    return app
+
